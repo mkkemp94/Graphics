ee0c4377c5ee232dda7276acb4035bc1285e1f75
Michael Kemp <mkemp@accu-time.com>
2017-08-14 16:39:17 -0400

    Draw texture with pixmap

--- core/src/com/mkemp/graphics/Graphics.java
+++ core/src/com/mkemp/graphics/Graphics.java
@@ -2,20 +2,29 @@ package com.mkemp.graphics;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Graphics extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture texture;
 	private Sprite sprite;
-	
+
+	private Texture texture;
+	private Pixmap pixmap;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
 		texture = new Texture(Gdx.files.internal("data/jet.png"));
+		//createPixmapTexture();
+
 		sprite = new Sprite(texture);
 	}
 
@@ -26,6 +35,8 @@ public class Graphics extends ApplicationAdapter {
 
 		batch.begin();
 		sprite.draw(batch);
+		//sprite.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		//sprite.draw(batch);
 		batch.end();
 	}
 	
@@ -33,5 +44,28 @@ public class Graphics extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		texture.dispose();
+	}
+
+	/**
+	 * Create a pixmap and assign it to a texture.
+	 */
+	private void createPixmapTexture() {
+		// A pixmap is a raw image in memory represented by pixels.
+		pixmap = new Pixmap(256, 128, Pixmap.Format.RGBA8888);
+		pixmap.setColor(Color.RED);
+		pixmap.fill();
+
+		// Draw two lines forming an x
+		pixmap.setColor(Color.BLACK);
+		pixmap.drawLine(0, 0, pixmap.getWidth()-1, pixmap.getHeight()-1);
+		pixmap.drawLine(0, pixmap.getHeight()-1, pixmap.getWidth()-1, 0);
+
+		// Draw a circle about the middle
+		pixmap.setColor(Color.YELLOW);
+		pixmap.drawCircle(pixmap.getWidth()/2, pixmap.getHeight()/2, pixmap.getHeight()/2 - 1);
+
+		// Assign pixmap to texture and dispose of it.
+		texture = new Texture(pixmap);
+		pixmap.dispose();
 	}
 }
